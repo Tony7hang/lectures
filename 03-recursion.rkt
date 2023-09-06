47db74ad328fc2d959a4f4b5f05f43d0a566ac22
Tony7hang <tzhang67@hawk.iit.edu>
2023-09-06 14:42:25 -0500

    Update 03-recursion.rkt

--- 03-recursion.rkt
+++ 03-recursion.rkt
@@ -8,14 +8,38 @@
 -----------------------------------------------------------------------------|#
 
 (define (println-times datum n)
-  (println datum))
+  (when (> n 0)
+    (println datum)
+    (println-times datum (sub1 n))))
 
 
 ;; integer summation
 (define (sum-to n)
-  (void))
+  (if (= n 0)
+      0
+      (+ n (sum-to (sub1 n)))))
+; O(n) runtime complexity
+; also O(n) space complexity -> each recursion req a new stack in stacks
 
-; (trace sum-to)
+; makes it show the steps ran whenever sum-to is called
+(trace sum-to)
+
+(trace-define (sum-to-acc n acc ) ; acc stands for "accumulator" tail recursion, go other way???
+  (if (= n 0)
+      acc
+      (sum-to-acc (sub1 n) (+ n acc))))
+
+; trace-define defines and also trace!
+; Racket performs TCO "Tail-Call Optimization"
+
+#| not working lol, just look in the completed notes for all the other ways to do this 
+(define (sum-to-acc-2 n)
+  (trace-define (sum i acc)
+    (if (= n 0)
+      acc
+    (sum (sub1 n) (+ n acc))))
+      (sum n 0))
+|#
 
 
 ;; Fibonacci series: 0 1 1 2 3 5 8 13 21 34 55 ...
@@ -24,6 +48,8 @@
       n
       (+ (fib (- n 1))
          (fib (- n 2)))))
+
+; (trace fib)
 
 
 
@@ -38,10 +64,6 @@
 
 
 (define (repeat n x)
-  (void))
-
-
-(define (concat l1 l2)
   (void))
 
 
